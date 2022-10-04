66ff7ccd5be18186210d89c4abaa1be14f61ab00
nfepb <nicolas.planteb@gmail.com>
2022-10-04 17:42:26 +0000

    Tests if there are evens in the list to return True. Checks if numbers of evens is even

--- evens.py
+++ evens.py
@@ -9,6 +9,14 @@ def even_number_of_evens(numbers):
     if isinstance(numbers, list):
         if numbers == []:
             return False
+        else:
+            evens = 0
+
+        for n in numbers:
+            if n % 2 == 0:
+                evens += 1
+
+        return evens % 2 == 0
     else:
         raise TypeError("A list was not passed into the function")
 
